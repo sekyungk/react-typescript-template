00085f310d824a0941bac4de41a6058a28cc47d9
sekyungk <sekyungk@outlook.com>
2020-10-02 20:23:38 +0900

    ADD styled-normalize

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { FC } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from './styles/theme';
+import { GlobalStyle } from './styles/global-style';
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <div>App.tsx</div>
     </ThemeProvider>
   );
